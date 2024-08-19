21f262069b6bbf2fc042ba813aa3e0220ec66f74
Yen-Chun,Lin <asd900606@gmail.com>
2024-08-19 18:06:33 +0800

    Style: looks like

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,52 @@
-
 function App() {
-
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Wordle</h1>
-    </>
-  )
+      <div className="flex flex-wrap justify-center content-center w-screen h-96 outline-dashed">
+        <h1>Wordle</h1>
+        <ul className="flex flex-wrap justify-center items-center mx-auto w-full">
+          <li className="w-12 h-12 border-2 border-black text-center content-center text-4xl">M</li>
+          <li className="w-12 h-12 border-2 border-black text-center content-center text-4xl">E</li>
+          <li className="w-12 h-12 border-2 border-black text-center content-center text-4xl">T</li>
+          <li className="w-12 h-12 border-2 border-black text-center content-center text-4xl">E</li>
+          <li className="w-12 h-12 border-2 border-black text-center content-center text-4xl">R</li>
+        </ul>
+        <ul className="flex flex-wrap justify-center items-center mx-auto w-full">
+          <li className="w-12 h-12 border-2 border-black"></li>
+          <li className="w-12 h-12 border-2 border-black"></li>
+          <li className="w-12 h-12 border-2 border-black"></li>
+          <li className="w-12 h-12 border-2 border-black"></li>
+          <li className="w-12 h-12 border-2 border-black"></li>
+        </ul>
+        <ul className="flex flex-wrap justify-center items-center mx-auto w-full">
+          <li className="w-12 h-12 border-2 border-black"></li>
+          <li className="w-12 h-12 border-2 border-black"></li>
+          <li className="w-12 h-12 border-2 border-black"></li>
+          <li className="w-12 h-12 border-2 border-black"></li>
+          <li className="w-12 h-12 border-2 border-black"></li>
+        </ul>
+        <ul className="flex flex-wrap justify-center items-center mx-auto w-full">
+          <li className="w-12 h-12 border-2 border-black"></li>
+          <li className="w-12 h-12 border-2 border-black"></li>
+          <li className="w-12 h-12 border-2 border-black"></li>
+          <li className="w-12 h-12 border-2 border-black"></li>
+          <li className="w-12 h-12 border-2 border-black"></li>
+        </ul>
+        <ul className="flex flex-wrap justify-center items-center mx-auto w-full">
+          <li className="w-12 h-12 border-2 border-black"></li>
+          <li className="w-12 h-12 border-2 border-black"></li>
+          <li className="w-12 h-12 border-2 border-black"></li>
+          <li className="w-12 h-12 border-2 border-black"></li>
+          <li className="w-12 h-12 border-2 border-black"></li>
+        </ul>
+        <ul className="flex flex-wrap justify-center items-center mx-auto w-full">
+          <li className="w-12 h-12 border-2 border-black"></li>
+          <li className="w-12 h-12 border-2 border-black"></li>
+          <li className="w-12 h-12 border-2 border-black"></li>
+          <li className="w-12 h-12 border-2 border-black"></li>
+          <li className="w-12 h-12 border-2 border-black"></li>
+        </ul>
+        
+      </div>
+  );
 }
 
-export default App
+export default App;
