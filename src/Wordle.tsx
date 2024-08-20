3c9a1940171dfdeb1342b0fff053815ee8d0156a
Yen-Chun,Lin <asd900606@gmail.com>
2024-08-20 14:28:12 +0800

    Feat: reset words

--- src/Wordle.tsx
+++ src/Wordle.tsx
@@ -5,13 +5,15 @@ import { reducer, initialState } from './reducer';
 const Wordle: React.FC = () => {
   const guessWord = "meter";
   const guessWordArray = guessWord.toUpperCase().split("");
-  console.log(guessWordArray);
 
   const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
-      if (e.key.length === 1 && e.key.match(/[a-z]/i) && state.guesses.length < 5) {
+        if (e.key === 'Backspace') {
+            dispatch({ type: 'RESET' })
+          }
+      else if (e.key.length === 1 && e.key.match(/[a-z]/i) && state.guesses.length < 5) {
         dispatch({ type: 'ADD_GUESS', guess: e.key.toUpperCase() });
       }
     };
@@ -22,13 +24,14 @@ const Wordle: React.FC = () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
   }, [state.guesses.length]);
+
   return (
     <>
       <div className="flex flex-row flex-wrap justify-center content-center w-screen h-96 outline-dashed">
         <h1>Wordle</h1>
         <ul className="flex flex-wrap justify-center items-center mx-auto w-full gap-1 mb-1">
           {Array.from({ length: 5 }).map((_, i) => (
-            <li className="w-12 h-12 border-2 border-black text-center content-center text-4xl" key={i}>{state.guesses[i] || ''}</li>
+            <li className="w-12 h-12 border-2 border-black text-center content-center text-4xl" key={i}>{state.guesses[i]}</li>
           ))}
         </ul>
       </div>
