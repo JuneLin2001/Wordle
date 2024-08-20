5f7d980d9c8ce985f28b4f3f92b01d02c7804fbd
Yen-Chun,Lin <asd900606@gmail.com>
2024-08-20 10:17:14 +0800

    Style: basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,60 @@
 function App() {
   return (
-      <div className="flex flex-wrap justify-center content-center w-screen h-96 outline-dashed">
-        <h1>Wordle</h1>
-        <ul className="flex flex-wrap justify-center items-center mx-auto w-full gap-1 mb-1">
-          <li className="w-12 h-12 border-2 border-black text-center content-center text-4xl">M</li>
-          <li className="w-12 h-12 border-2 border-black text-center content-center text-4xl">E</li>
-          <li className="w-12 h-12 border-2 border-black text-center content-center text-4xl">T</li>
-          <li className="w-12 h-12 border-2 border-black text-center content-center text-4xl">E</li>
-          <li className="w-12 h-12 border-2 border-black text-center content-center text-4xl">R</li>
-        </ul>
-        <ul className="flex flex-wrap justify-center items-center mx-auto w-full gap-1 mb-1">
-          <li className="w-12 h-12 border-2 border-black"></li>
-          <li className="w-12 h-12 border-2 border-black"></li>
-          <li className="w-12 h-12 border-2 border-black"></li>
-          <li className="w-12 h-12 border-2 border-black"></li>
-          <li className="w-12 h-12 border-2 border-black"></li>
-        </ul>
-        <ul className="flex flex-wrap justify-center items-center mx-auto w-full gap-1 mb-1">
-          <li className="w-12 h-12 border-2 border-black"></li>
-          <li className="w-12 h-12 border-2 border-black"></li>
-          <li className="w-12 h-12 border-2 border-black"></li>
-          <li className="w-12 h-12 border-2 border-black"></li>
-          <li className="w-12 h-12 border-2 border-black"></li>
-        </ul>
-        <ul className="flex flex-wrap justify-center items-center mx-auto w-full gap-1 mb-1">
-          <li className="w-12 h-12 border-2 border-black"></li>
-          <li className="w-12 h-12 border-2 border-black"></li>
-          <li className="w-12 h-12 border-2 border-black"></li>
-          <li className="w-12 h-12 border-2 border-black"></li>
-          <li className="w-12 h-12 border-2 border-black"></li>
-        </ul>
-        <ul className="flex flex-wrap justify-center items-center mx-auto w-full gap-1 mb-1">
-          <li className="w-12 h-12 border-2 border-black"></li>
-          <li className="w-12 h-12 border-2 border-black"></li>
-          <li className="w-12 h-12 border-2 border-black"></li>
-          <li className="w-12 h-12 border-2 border-black"></li>
-          <li className="w-12 h-12 border-2 border-black"></li>
-        </ul>
-        <ul className="flex flex-wrap justify-center items-center mx-auto w-full gap-1 mb-1">
-          <li className="w-12 h-12 border-2 border-black"></li>
-          <li className="w-12 h-12 border-2 border-black"></li>
-          <li className="w-12 h-12 border-2 border-black"></li>
-          <li className="w-12 h-12 border-2 border-black"></li>
-          <li className="w-12 h-12 border-2 border-black"></li>
-        </ul>
-        
-      </div>
+    <div className="flex flex-wrap justify-center content-center w-screen h-96 outline-dashed">
+      <h1>Wordle</h1>
+      <ul className="flex flex-wrap justify-center items-center mx-auto w-full gap-1 mb-1">
+        <li className="w-12 h-12 border-2 border-black text-center content-center text-4xl">
+          M
+        </li>
+        <li className="w-12 h-12 border-2 border-black text-center content-center text-4xl">
+          E
+        </li>
+        <li className="w-12 h-12 border-2 border-black text-center content-center text-4xl">
+          T
+        </li>
+        <li className="w-12 h-12 border-2 border-black text-center content-center text-4xl">
+          E
+        </li>
+        <li className="w-12 h-12 border-2 border-black text-center content-center text-4xl">
+          R
+        </li>
+      </ul>
+      <ul className="flex flex-wrap justify-center items-center mx-auto w-full gap-1 mb-1">
+        <li className="w-12 h-12 border-2 border-black"></li>
+        <li className="w-12 h-12 border-2 border-black"></li>
+        <li className="w-12 h-12 border-2 border-black"></li>
+        <li className="w-12 h-12 border-2 border-black"></li>
+        <li className="w-12 h-12 border-2 border-black"></li>
+      </ul>
+      <ul className="flex flex-wrap justify-center items-center mx-auto w-full gap-1 mb-1">
+        <li className="w-12 h-12 border-2 border-black"></li>
+        <li className="w-12 h-12 border-2 border-black"></li>
+        <li className="w-12 h-12 border-2 border-black"></li>
+        <li className="w-12 h-12 border-2 border-black"></li>
+        <li className="w-12 h-12 border-2 border-black"></li>
+      </ul>
+      <ul className="flex flex-wrap justify-center items-center mx-auto w-full gap-1 mb-1">
+        <li className="w-12 h-12 border-2 border-black"></li>
+        <li className="w-12 h-12 border-2 border-black"></li>
+        <li className="w-12 h-12 border-2 border-black"></li>
+        <li className="w-12 h-12 border-2 border-black"></li>
+        <li className="w-12 h-12 border-2 border-black"></li>
+      </ul>
+      <ul className="flex flex-wrap justify-center items-center mx-auto w-full gap-1 mb-1">
+        <li className="w-12 h-12 border-2 border-black"></li>
+        <li className="w-12 h-12 border-2 border-black"></li>
+        <li className="w-12 h-12 border-2 border-black"></li>
+        <li className="w-12 h-12 border-2 border-black"></li>
+        <li className="w-12 h-12 border-2 border-black"></li>
+      </ul>
+      <ul className="flex flex-wrap justify-center items-center mx-auto w-full gap-1 mb-1">
+        <li className="w-12 h-12 border-2 border-black"></li>
+        <li className="w-12 h-12 border-2 border-black"></li>
+        <li className="w-12 h-12 border-2 border-black"></li>
+        <li className="w-12 h-12 border-2 border-black"></li>
+        <li className="w-12 h-12 border-2 border-black"></li>
+      </ul>
+    </div>
   );
 }
 
