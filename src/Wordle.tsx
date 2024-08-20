91aacb25a4c14b1a2326c25ec4affe2492273eef
Yen-Chun,Lin <asd900606@gmail.com>
2024-08-20 19:12:35 +0800

    Style: colors

--- src/Wordle.tsx
+++ src/Wordle.tsx
@@ -37,43 +37,43 @@ const Wordle: React.FC = () => {
   }, [state.guessedWords.length, state.isCorrect]);
 
   return (
-    <div className="flex flex-row flex-wrap justify-center content-center w-screen h-96 outline-dashed">
-      {Array.from({ length: 6 }).map((_, rowIndex) => (
-        <ul
-          key={rowIndex}
-          className="flex justify-center items-center mx-auto w-full gap-1 mb-1"
-        >
-          {Array.from({ length: 5 }).map((_, colIndex) => {
-            const guessedWord = state.guessedWords[rowIndex];
-            const displayValue =
-              guessedWord?.guess[colIndex] ||
-              (rowIndex === state.guessedWords.length
-                ? state.guesses[colIndex] || ""
-                : "");
+      <div className="flex flex-row flex-wrap justify-center content-center w-screen h-96 outline-dashed">
+        {Array.from({ length: 6 }).map((_, rowIndex) => (
+          <ul
+            key={rowIndex}
+            className="flex justify-center items-center mx-auto w-full gap-1 mb-1"
+          >
+            {Array.from({ length: 5 }).map((_, colIndex) => {
+              const guessedWord = state.guessedWords[rowIndex];
+              const displayValue =
+                guessedWord?.guess[colIndex] ||
+                (rowIndex === state.guessedWords.length
+                  ? state.guesses[colIndex] || ""
+                  : "");
 
-            const color = guessedWord?.colors[colIndex] || "transparent";
-            const bgColor =
-              color === "green"
-                ? "bg-green-500"
-                : color === "yellow"
-                ? "bg-yellow-500"
-                : color === "gray"
-                ? "bg-gray-300"
-                : "bg-white";
+              const color = guessedWord?.colors[colIndex] || "transparent";
+              const bgColor =
+                color === "green"
+                  ? "bg-green-500"
+                  : color === "yellow"
+                  ? "bg-yellow-500"
+                  : color === "gray"
+                  ? "bg-gray-300"
+                  : "bg-white";
 
-            return (
-              <li
-                key={colIndex}
-                className={`w-12 h-12 border-2 border-black text-center content-center text-4xl ${bgColor}`}
-              >
-                {displayValue}
-              </li>
-            );
-          })}
-        </ul>
-      ))}
-      <h2>Answer is {state.word}</h2>
-    </div>
+              return (
+                <li
+                  key={colIndex}
+                  className={`w-12 h-12 border-2 border-black text-center content-center text-4xl ${bgColor}`}
+                >
+                  {displayValue}
+                </li>
+              );
+            })}
+          </ul>
+        ))}
+        <h2>Answer is {state.word}</h2>
+      </div>
   );
 };
 
