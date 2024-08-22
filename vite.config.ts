5c34a0b0d1b45810965896215e76d993edad35b7
Yen-Chun,Lin <asd900606@gmail.com>
2024-08-22 14:43:33 +0800

    Fix: add secret api key
    Fix: add secret api key

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/Wordle",
   plugins: [react()],
-})
+  define: {
+    "process.env": {
+      VITE_FIREBASE_API_KEY: process.env.VITE_FIREBASE_API_KEY,
+      VITE_FIREBASE_APP_ID: process.env.VITE_FIREBASE_APP_ID,
+      VITE_FIREBASE_AUTH_DOMAIN: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+      VITE_FIREBASE_MESSAGING_SENDER_ID:
+        process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+      VITE_FIREBASE_PROJECT_ID: process.env.VITE_FIREBASE_PROJECT_ID,
+      VITE_FIREBASE_STORAGE_BUCKET: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+    },
+  },
+});
